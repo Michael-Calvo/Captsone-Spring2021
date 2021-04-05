daa6f795e5ff887d6ab7f419dc7f4bdbd5b948f7
michael-calvo <calvo.michael1@gmail.com>
2021-04-04 22:29:51 -0400

    Landing Page folder rework

--- FrontEndPOS/src/app/app-routing.module.ts
+++ FrontEndPOS/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './landing_page_components/profile/profile.component';
-import { NavTestComponent } from './landing_page_components/nav-test/nav-test.component';
+import { ProfileComponent } from './landing_components/profile/profile.component';
+import { SidebarComponent } from './landing_components/sidebar/sidebar.component';
 import { InventoryPageComponent } from './inventory_components/inventory-page/inventory-page.component';
 
 const routes: Routes = [
