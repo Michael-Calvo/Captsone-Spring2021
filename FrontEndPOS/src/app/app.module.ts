cecca43ed016e6109d58f1ba39646263748baf9b
michael-calvo <calvo.michael1@gmail.com>
2021-04-04 22:33:05 -0400

    Revert "Landing Page folder rework"
    This reverts commit daa6f795e5ff887d6ab7f419dc7f4bdbd5b948f7.

--- FrontEndPOS/src/app/app.module.ts
+++ FrontEndPOS/src/app/app.module.ts
@@ -4,21 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavTestComponent } from './landing_page_components/nav-test/nav-test.component';
+import { ProfileComponent } from './landing_page_components/profile/profile.component';
+import { TitleComponentComponent } from './landing_page_components/title-component/title-component.component';
 import { InventoryPageComponent } from './inventory_components/inventory-page/inventory-page.component';
 import { InventoryListComponent } from './inventory_components/inventory-list/inventory-list.component';
-import { TitleComponent } from './landing_components/title/title.component';
-import { SidebarComponent } from './landing_components/sidebar/sidebar.component';
-import { ProfileComponent } from './landing_components/profile/profile.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavTestComponent,
+    ProfileComponent,
+    TitleComponentComponent,
     InventoryPageComponent,
-    InventoryListComponent,
-    TitleComponent,
-    SidebarComponent,
-    ProfileComponent
+    InventoryListComponent
   ],
   imports: [
     BrowserModule,
