a93f540976b758fdc9d51b1961d1cd97f514e58e
iwilhelmsen <i.wilhelmsen13@gmail.com>
2021-04-05 15:58:20 -0400

    fixing errors in script

--- database/dropCreateUser.sql
+++ database/dropCreateUser.sql
@@ -1,3 +1,3 @@
 drop user if exists 'posuser'@'localhost';
-create user 'posuser'@'localhost' identified by 'Burnt4Pizzas!';
+create user 'posuser'@'localhost' identified by '1q2w3e4r5t';
 grant execute on posdb.* to 'posuser'@'localhost';
