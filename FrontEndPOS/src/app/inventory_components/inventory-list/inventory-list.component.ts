8ebeacf9ba4787a10ca8783d05e35137df49b8f8
Darthscifiguy <77691540+rdabrams@users.noreply.github.com>
2021-04-04 21:28:18 -0400

    inventory addons

--- FrontEndPOS/src/app/inventory_components/inventory-list/inventory-list.component.ts
+++ FrontEndPOS/src/app/inventory_components/inventory-list/inventory-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { InventoryClass } from '../inventory_service/inventory-class';
+import { MenuItem } from '../inventory_service/menu-item';
 import { InventoryService } from '../inventory_service/inventory.service';
+import { Transporter } from '../inventory_service/transporter';
 
 @Component({
   selector: 'app-inventory-list',
@@ -9,12 +10,107 @@ import { InventoryService } from '../inventory_service/inventory.service';
 })
 export class InventoryListComponent implements OnInit {
 
-  public inventoryItems:InventoryClass[] = [];
+  public inventoryItems:MenuItem[] = [];
+  public transporterArr:Transporter[] = [];
 
   constructor(private inventoryService: InventoryService) { }
 
   ngOnInit(): void {
-    this.inventoryService.getdata().subscribe(data => { this.inventoryItems = data;})
+
+    this.inventoryItems = [
+      {
+        function: "Read",
+        object: "MenuItem",
+        itemName: "Hamburger",
+        menuID: 1235,
+        price:  5.99,
+        priorityScore: 2,
+        executionTime:  10
+      },
+      {
+        function: "Read",
+        object: "MenuItem",
+        itemName: "Ham and Cheese",
+        menuID: 1236,
+        price:  2.99,
+        priorityScore: 3,
+        executionTime:  2
+      },
+      {
+        function: "Read",
+        object: "MenuItem",
+        itemName: "Omelet",
+        menuID: 1237,
+        price:  4.99,
+        priorityScore: 2,
+        executionTime:  7
+      }
+
+    ]
+    
+    //this.inventoryService.getdata().subscribe(data => { this.inventoryItems = data;})
+    //this.inventoryService.getdata().subscribe(data => { this.transporterArr = data;})
   }
 
+  createPost(){
+    const data : MenuItem  = {
+      function: "API_CREATE",
+      object: "MenuItem",
+      itemName: "BLT",
+      menuID: 1234,
+      price:  4.99,
+      priorityScore: 1,
+      executionTime:  5
+    }
+    this.inventoryService.createPost(data).subscribe(inventoryClass => { 
+      this.inventoryItems.push(inventoryClass);
+    });
+  }
+
+  deleteRow(id){
+    for(let i = 0; i < this.inventoryItems.length; ++i){
+        if (this.inventoryItems[i].menuID === id) {
+            this.inventoryItems.splice(i,1);
+        }
+    }
+  }
+  addBLT(){
+    var data : MenuItem  = {
+      function: "Create",
+      object: "MenuItem",
+      itemName: "BLT",
+      menuID: 1238,
+      price:  4.99,
+      priorityScore: 1,
+      executionTime:  5
+    }
+    for(let i = 0; i < this.inventoryItems.length; ++i){
+      if (this.inventoryItems[i].menuID === data.menuID) {
+          data.menuID = data.menuID + 1
+      }
+  }
+    this.inventoryItems.push(data);
+  }
+
+  addItem(){
+    const data : Transporter = {
+      function: "API_CREATE",
+      object: "MenuItem",
+      payload: [
+        "Grilled Cheese",
+        1239,
+        2.99,
+        1,
+        5
+      ]
+    }
+    for(let i = 0; i < this.transporterArr.length; ++i){
+      if (this.transporterArr[i].payload[1] === data.payload[1]) {
+        data.payload[1] = data.payload[1] + 1
+      }
+  }
+  this.inventoryService.createTransporterPost(data).subscribe(transporter => { 
+    this.transporterArr.push(transporter);
+  });
+  }
 }
---
+++ FrontEndPOS/src/app/inventory_components/inventory_service/model-item.spec.ts
@@ -0,0 +1,7 @@
+import { ModelItem } from './model-item';
+
+describe('ModelItem', () => {
+  it('should create an instance', () => {
+    expect(new ModelItem()).toBeTruthy();
+  });
+});
