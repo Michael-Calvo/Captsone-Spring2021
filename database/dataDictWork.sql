ae89ae3888801d0a2af25041ca771bc26009775d
iwilhelmsen <i.wilhelmsen13@gmail.com>
2021-04-05 22:31:45 -0400

    Adding in some doc

--- database/dataDictWork.sql
+++ database/dataDictWork.sql
@@ -1,1 +1,5 @@
-select table_name from information_schema.tables WHERE table_schema = 'posdb';
+SELECT TABLE_NAME 'Table', COLUMN_NAME 'Field', COLUMN_TYPE 'Type', IS_NULLABLE 'Nullable',
+  COLUMN_KEY 'Key', COLUMN_DEFAULT 'Default', EXTRA 'Extra'
+FROM information_schema.columns
+WHERE table_schema = 'posdb'
+ORDER BY TABLE_NAME;
