ed52e324e8ea56de161381b4308147d8a3182caf
jtmanni2 <55925709+jtmanni2@users.noreply.github.com>
2021-04-14 19:41:45 -0400

    added current time

--- FrontEndPOS/src/app/scheduling_components/scheduling-page/scheduling-page.component.ts
+++ FrontEndPOS/src/app/scheduling_components/scheduling-page/scheduling-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { ClockIn } from '../scheduling-service/clock-in';
 
 @Component({
   selector: 'app-scheduling-page',
@@ -8,12 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class SchedulingComponent implements OnInit {
   
   
-
+  time = new Date();
+  currentTime = '';
   constructor() {
     
    }
+   ClockIn() {
+     alert(this.currentTime);
+   }
+   ClockOut(){
+     alert(this.currentTime)
+   }
 
-  ngOnInit(): void {
+  ngOnInit()  {
+    setInterval(() => {
+      this.time = new Date();
+      this.currentTime = formatDate(this.time, 'MM-dd-yyyy hh:mm:ss', 'en-US');
+    }, 1000);
   }
-
+ 
+  
 }
