cfadfb49f566ea06d24d4363c29df170898d6f94
michael-calvo <calvo.michael1@gmail.com>
2021-04-20 17:08:17 -0400

    Final Push before merge is needed.

--- FrontEndPOS/src/app/app.module.ts
+++ FrontEndPOS/src/app/app.module.ts
@@ -7,9 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InventoryPageComponent } from './inventory_components/inventory-page/inventory-page.component';
 import { InventoryListComponent } from './inventory_components/inventory-list/inventory-list.component';
-import { InventoryItemComponent } from './inventory_components/inventory-item/inventory-item.component';
-import { TitleComponent } from './landingPage_components/title/title.component';
-import { SidebarComponent } from './landingPage_components/sidebar/sidebar.component';
 import {ProfileComponent} from './landingPage_components/profile/profile.component';
 import {AngularFireModule} from "@angular/fire";
 import {AngularFireAuthModule} from "@angular/fire/auth";
@@ -25,8 +22,7 @@ import {SidebarComponent} from './nav_components/sidebar/sidebar.component';
     AppComponent,
     InventoryPageComponent,
     InventoryListComponent,
-    InventoryItemComponent,
-    TitleComponent,
+    ProfileComponent,
     SidebarComponent,
     WelcomePageComponent,
     AddInventoryItemComponent,
