b5a18c7365a675bd08457c35377083074db4ee92
michael-calvo <calvo.michael1@gmail.com>
2021-04-20 16:31:35 -0400

    Revert "Update on Mock User"
    This reverts commit f5272b12d1301bbbf544fac161e9f3e731893929.

--- FrontEndPOS/src/app/nav_components/sidebar/sidebar.component.ts
+++ FrontEndPOS/src/app/nav_components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../auth_service/auth.service";
+import {AuthService} from "../../landingPage_components/auth_service/auth.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,13 +7,10 @@ import {AuthService} from "../../auth_service/auth.service";
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  user1;
 
   constructor( public authService: AuthService) { }
 
   ngOnInit(): void {
-    this.user1 = this.authService.getUserData();
-
   }
 
 
