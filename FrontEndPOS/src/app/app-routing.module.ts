5d9b9ce2ef3152debc0f1f578701b1f0bd3f96b7
Darthscifiguy <77691540+rdabrams@users.noreply.github.com>
2021-04-21 13:27:45 -0400

    UI updates

--- FrontEndPOS/src/app/app-routing.module.ts
+++ FrontEndPOS/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InventoryPageComponent } from './inventory_components/inventory-page/inventory-page.component';
+import { InventoryListComponent} from './inventory_components/inventory-list/inventory-list.component';
 import {ProfileComponent} from './landingPage_components/profile/profile.component';
 import {WelcomePageComponent} from './landingPage_components/welcome-page/welcome-page.component';
 import {AddInventoryItemComponent} from './inventory_components/add-inventory-item/add-inventory-item.component';
 const routes: Routes = [
   {path: '',component: WelcomePageComponent},
   {path: 'welcome-page',component: WelcomePageComponent},
-  {path: 'inventory',component: InventoryPageComponent},
+  {path: 'inventory',component: InventoryListComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'add-item', component: AddInventoryItemComponent}
 ];
